99eb82877f6e7d2ac1e8ce43f1e53085b3b1527b
Thomas Murphy <Tom@Thomass-MacBook-Air.local>
2017-08-18 13:29:00 -0400

    spotify working

--- liri.js
+++ liri.js
@@ -1,24 +1,21 @@
 var request = require('request');
-
 var fs = require('fs');
-
 var action = process.argv[2];
 var value = process.argv[3];
-var result = '';
 
 switch (action) {
   case 'my-tweets':
-  tweets();
-  break;
+    tweets();
+    break;
   case 'spotify-this-song':
-  spotify();
-  break;
+    spotify();
+    break;
   case 'movie-this':
-  movie();
-  break;
+    movie();
+    break;
   case 'do-what-it-says':
-  says();
-  break;
+    says();
+    break;
 }
 
 function tweets() {
@@ -26,15 +23,17 @@ function tweets() {
   var keys = require('./keys.js');
   var client = new Twitter(keys.twitterKeys);
   var params = {
-    screen_name: 'sewer_weasel', 
+    screen_name: 'sewer_weasel',
     count: 10
   };
   client.get('statuses/user_timeline', params, (error, tweets, response) => {
     if (!error && response.statusCode === 200) {
       console.log('Last 10 Tweets:')
       for (i = 0; i < tweets.length; i++) {
+        console.log('==================================================');
         console.log('sewerWeasel: ' + tweets[i].text);
         console.log('Tweeted On: ' + tweets[i].created_at);
+        console.log('==================================================');
         fs.appendFile('log.txt', ('sewerWeasel: ' + tweets[i].text + ' | ' + tweets[i].created_at + ' \n'), (err) => {
           if (err) {
             return console.log(err);
@@ -42,11 +41,29 @@ function tweets() {
         });
       }
     };
-  })
+  });
 };
 
-// Spotify
-// Client ID
-// ea48b89926cb46dea4abc0f5712eb362
-// Client Secret
-// 9cc83f9b1417415fba0a33868a7819a0
+function spotify() {
+  var Spotify = require('node-spotify-api');
+  var spotify = new Spotify({
+    id: 'ea48b89926cb46dea4abc0f5712eb362',
+    secret: '9cc83f9b1417415fba0a33868a7819a0'
+  });
+  // if (value === null) {
+  //   output = "The Sign";
+  // }
+  spotify.search({ type: 'track', query: value }, (err, data) => {
+    // if (!error && response.statusCode === 200) {
+      console.log('==================================================');
+      console.log("Artist: " + data.tracks.items[0].artists[0].name);
+      console.log("Song Name: " + data.tracks.items[0].name);
+      console.log("Album: " + data.tracks.items[0].album.name);
+      console.log("Spotify Preview Link: " + data.tracks.items[0].external_urls.spotify);
+      console.log('==================================================');
+      if (err) {
+        return console.log(err);
+      }
+    // }
+  });
+};
