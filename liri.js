69425d7e00d54717238d0de17209220db0531bd0
Thomas Murphy <Tom@Thomass-MacBook-Air.local>
2017-08-18 21:52:36 -0400

    starting movie

--- liri.js
+++ liri.js
@@ -25,7 +25,7 @@ function tweets() {
     screen_name: 'sewer_weasel',
     count: 10
   };
-  client.get('statuses/user_timeline', params, (error, tweets, response) => {
+  client.get('statuses/user_timeline', params, (err, tweets, response) => {
     if (!error && response.statusCode === 200) {
       console.log('Last 10 Tweets:')
       for (i = 0; i < tweets.length; i++) {
@@ -33,9 +33,9 @@ function tweets() {
         console.log('sewerWeasel: ' + tweets[i].text);
         console.log('Tweeted On: ' + tweets[i].created_at);
         console.log('==================================================');
-        fs.appendFile('log.txt', ('sewerWeasel: ' + tweets[i].text + ' | ' + tweets[i].created_at + ' \n'), (error) => {
-          if (error) {
-            return console.log(error);
+        fs.appendFile('log.txt', ('sewerWeasel: ' + tweets[i].text + ' | ' + tweets[i].created_at + ' \n'), (err) => {
+          if (err) {
+            return console.log(err);
           }
         });
       }
@@ -49,10 +49,10 @@ function spotify() {
     id: 'ea48b89926cb46dea4abc0f5712eb362',
     secret: '9cc83f9b1417415fba0a33868a7819a0'
   });
-  // if (value === undefined) {
-  //   output = 'The Sign';
+  // if (value === null) {
+  //   value = 'The Sign';
   // }
-  spotify.search({ type: 'track', query: value }, (error, data) => {
+  spotify.search({ type: 'track', query: value, limit: 5 }, (err, data) => {
     // if (!error && response.statusCode === 200) {
       console.log('==================================================');
       console.log("Artist: " + data.tracks.items[0].artists[0].name);
@@ -60,17 +60,71 @@ function spotify() {
       console.log("Album: " + data.tracks.items[0].album.name);
       console.log("Spotify Preview Link: " + data.tracks.items[0].external_urls.spotify);
       console.log('==================================================');
-      fs.appendFile('log.txt', ('\nArtist: ' + data.tracks.items[0].artists[0].name + '\nSong: ' + data.tracks.items[0].name + '\nPreview Link: ' + data.tracks.items[0].preview_url + '\nAlbum: ' + data.tracks.items[0].album.name + '\n')), (error) => {
-        if (error) {
-          return console.log(error);
+      fs.appendFile('log.txt', ('\nArtist: ' + data.tracks.items[0].artists[0].name + '\nSong: ' + data.tracks.items[0].name + '\nPreview Link: ' + data.tracks.items[0].preview_url + '\nAlbum: ' + data.tracks.items[0].album.name + '\n')), (err) => {
+        if (err) {
+          return console.log(err);
         }
       }
     // }
   });
 };
 
-
+function movie() {
+  var request = require('request');
+  // if (value === null) {
+  //   value = 'Mr. Nobody';
+  // }
+  request('http://www.omdbapi.com/?t=' + value + '&tomatoes=true&r=json', (err, response, body) => {
+    // if (!error && response.statusCode == 200) {
+      console.log('==================================================');
+      console.log('Title: ' + JSON.parse(body).Title);
+      console.log('Year: ' + JSON.parse(body).Year);
+      console.log('IMDb Rating: ' + JSON.parse(body).imdbRating);
+      console.log('Rotten Tomatoes Rating: ' + JSON.parse(body).tomatoRating);
+      console.log('Country: ' + JSON.parse(body).Country);
+      console.log('Language: ' + JSON.parse(body).Language);
+      console.log('Plot: ' + JSON.parse(body).Plot);
+      console.log('Actors: ' + JSON.parse(body).Actors);
+      console.log('==================================================');
+      fs.appendFile('log.txt', ('\nTitle: ' + jsonBody.Title + '\r\nYear: ' + jsonBody.Year + '\r\nIMDb Rating: ' + jsonBody.imdbRating + '\r\nCountry: ' + jsonBody.Country + '\r\nLanguage: ' + jsonBody.Language + '\r\nPlot: ' + jsonBody.Plot + '\r\nActors: ' + jsonBody.Actors + '\r\nRotten Tomatoes Rating: ' + jsonBody.tomatoRating + '\r\nRotten Tomatoes URL: ' + jsonBody.tomatoURL + '\n'), (err) => {
+        if (err) {
+          return console.log(err);
+        }
+      });
+    })
+};
 
 // function movie() {
-//   var request = require('request');
+//   var request = require("request");
+//   var nodeArgs = process.argv;
+//   var movieName = "";
+//   for (var i = 2; i < nodeArgs.length; i++) {
+//     if (i > 2 && i < nodeArgs.length) {
+//       movieName = movieName + "+" + nodeArgs[i];
+//     } else {
+//       movieName += nodeArgs[i];
+//     }
+//   }
+//   var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+//   console.log(queryUrl);
+//   request(queryUrl, (error, response, body) => {
+//     if (!error && response.statusCode === 200) {
+//       console.log('==================================================');
+//       console.log('Title: ' + JSON.parse(body).Title);
+//       console.log('Year: ' + JSON.parse(body).Year);
+//       console.log('IMDb Rating: ' + JSON.parse(body).imdbRating);
+//       console.log('Country: ' + JSON.parse(body).Country);
+//       console.log('Language: ' + JSON.parse(body).Language);
+//       console.log('Plot: ' + JSON.parse(body).Plot);
+//       console.log('Actors: ' + JSON.parse(body).Actors);
+//       console.log('Rotten Tomatoes Rating: ' + JSON.parse(body).tomatoRating);
+//       console.log('Rotten Tomatoes URL: ' + JSON.parse(body).tomatoURL);
+//       console.log('==================================================');
+//       fs.appendFile('log.txt', ('\nTitle: ' + JSON.parse(body).Title + '\r\nYear: ' + JSON.parse(body).Year + '\r\nIMDb Rating: ' + JSON.parse(body).imdbRating + '\r\nCountry: ' + JSON.parse(body).Country + '\r\nLanguage: ' + JSON.parse(body).Language + '\r\nPlot: ' + JSON.parse(body).Plot + '\r\nActors: ' + JSON.parse(body).Actors + '\r\nRotten Tomatoes Rating: ' + JSON.parse(body).tomatoRating + '\r\nRotten Tomatoes URL: ' + JSON.parse(body).tomatoURL + '\n'), (err) => {
+//         if (err) {
+//           return console.log(err);
+//         }
+//       });
+//     }
+//   });
 // }
